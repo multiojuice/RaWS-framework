454939ed7bd2fb2602b49e242f3fcd876fdde4a9
Owen Sullivan <multiojuice@gmail.com>
2019-01-17 22:49:18 -0500

    Set up the base of the app as a thread

--- src/main/java/com/multiojuice/RaWsFramework/RaWsApp.java
+++ src/main/java/com/multiojuice/RaWsFramework/RaWsApp.java
@@ -1,11 +1,26 @@
 package com.multiojuice.RaWsFramework;
 
-/**
- * Hello world!
- *
- */
-public class RaWsApp {
-    public void printSomething() {
-        System.out.println("Hi there");
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class RaWsApp extends Thread {
+    private ServerSocket server;
+
+    public RaWsApp() {
+        try {
+            server = new ServerSocket(8080);
+        } catch(Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public void run() {
+        while(true) {
+            try (Socket socket = server.accept()) {
+                System.out.println("Got a request");
+            } catch(Exception e) {
+                System.out.println(e);
+            }
+        }
     }
 }
